fa9970819c3dcc8a96fc85bea26fc0b1f893a601
kevintamir <101955380+kevintamir@users.noreply.github.com>
2024-08-11 22:13:56 -0400

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+const menu = document.getElementById("menu");
+Array.from(document.getElementsByClassName("menu-item")).forEach(
+  (item, index) => {
+    item.onmouseover = () => {
+      menu.dataset.activeIndex = index;
+    };
+  }
+);
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("card");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
